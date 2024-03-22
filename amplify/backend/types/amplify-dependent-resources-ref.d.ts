8c8878e91f7156ff2ee5d8957ca8d4b7facbbf61
Vladimir Pasquier <1112241+vpasquier@users.noreply.github.com>
2024-03-22 18:44:37 -0400

    Add amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "lifeadapt": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "lifeadaptbackendf511c7e2": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
